bf64ed3486761074bc72639ce5b00970e3133817
Stellshi <simplepro47@gmail.com>
2021-05-29 11:18:52 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Circle 
+    {
+        static void Main()
+        {
+            double radius;
+            double area;
+
+            radius = 10.0;
+            area = radius * radius * 3.1416;
+
+            Console.WriteLine("Площадь равна " + area);
+        }
+    }
+}
